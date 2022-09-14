17c32f76d036785a836422c03e0fc5f5be881f7d
hyozzi92 <gytjdzja@gmail.com>
2022-09-14 16:17:50 +0900

    leftSideBar 1st modified

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
 // import React, { useState } from "react";
 import MainTrend from '../pages/kimdaeho/MainTest/MainTrend';
+import LeftSideBar from '../pages/hyosung/leftSideBar';
+import { useState, useEffect } from 'react';
 
 function Main() {
+  useEffect(() => {
+    fetch('/data/profile.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setProfile(...data);
+      });
+  }, []);
+  const [profile, setProfile] = useState({});
+  console.log(profile);
   return (
-    <div>
+    <div className="main">
       <MainTrend></MainTrend>
+      <LeftSideBar profile={profile} />
     </div>
   );
 }
