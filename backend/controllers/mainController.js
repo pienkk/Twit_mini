d137c29be691e871b879504ccbc8896b21afb9f6
es <mascoeskim@gmail.com>
2022-09-15 18:46:47 +0900

    메인서비스 미완성

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -10,11 +10,12 @@ const mainFeed = asyncWrap(async (req, res) => {
       return res.status(400).json({ message: "KEY_ERROR" });
     }
     
+    
     let mainData = await mainService.mainFeed(
       user_id
      
     );
-    res.status(201).json(mainData);
+     await res.status(201).json({ message: "오늘 백엔드 영업 종료합니다" });
   });
 
   module.exports = { mainFeed };
--- backend/models/mainDao.js
+++ backend/models/mainDao.js
@@ -1,25 +1,39 @@
 const database = require("./orm");
 
-const mainFeed = async () => {
-    try {
-      return await database.query(
-        `SELECT tweets.user_id,
-        tweets.id,
-        tweets.replyTF,
-        tweets.content,
-        tweets.content_img,
-        tweets.reply_at,
-        tweets.create_at,
-        tweets.tweet_for,
-        likes.user_id AS "like"
- FROM users JOIN tweets ON users.id = tweets.user_id
- LEFT JOIN likes ON tweets.id = likes.tweet_id;`
-      );
-    } catch (err) {
-      const error = new Error("INVALID_DATA_INPUT");
-      error.statusCode = 500;
-      throw error;
-    }
-  };
-  
-  module.exports = { mainFeed };
+const mainFeed = async (user_id) => {
+  try {
+    return await database.query(
+      `select tweets.id, 
+      users.profile_id, 
+      users.profile_nickname, 
+      tweets.replyTF, 
+      tweets.content, 
+      tweets.content_img, 
+      likes.tweet_id AS "likeEx" from tweets
+      inner join follows on follows.follow_to = tweets.user_id 
+      inner join users on tweets.user_id = users.id 
+      left join likes on likes.tweet_id = tweets.id AND likes.user_id = ? WHERE follow_from = ?;
+      `,[user_id, user_id]
+
+    );
+  } catch (err) {
+    const error = new Error("INVALID_DATA_INPUT");
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
+const replyCount = async (user_id) => {
+  try{
+    return await database.query(`SELECT tweets.id
+    from tweets WHERE replyTF=1
+    `
+    );
+  }catch (err) {
+    const error = new Error("INVALID_DATA_INPUT");
+    error.statusCode = 500;
+    throw error;
+  }
+} 
+
+module.exports = { mainFeed, replyCount};
