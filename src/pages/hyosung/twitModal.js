20d94de4c38d7ab9679576cbf86d77f8e27b992a
kimdaeho <eogh773@naver.com>
2022-09-18 17:12:49 +0900

    test

--- src/pages/hyosung/twitModal.js
+++ src/pages/hyosung/twitModal.js
@@ -1,25 +1,21 @@
 import React, { useState } from 'react';
 import Modal from '../seunghoon/MainFeed/Modal';
 import ModalPortal from '../seunghoon/MainFeed/Portal';
-
 function TwitModal() {
   const [modal, setModal] = useState(false);
-  const handleModal = () => {
-    setModal(false);
+  const handleModal = event => {
+    if (modal === false) {
+      setModal(true);
+    }
+    if (event.target.className === 'modalContainer') {
+      setModal(false);
+    }
   };
   return (
-    <>
-      <div
-        onClick={() => {
-          setModal(true);
-        }}
-        className="twit"
-      >
-        트윗하기
-      </div>
-      <ModalPortal>{modal && <Modal />}</ModalPortal>
-    </>
+    <div onClick={handleModal}>
+      <div className="twit">트윗하기</div>
+      <ModalPortal>{modal && <Modal isComment={false} />}</ModalPortal>
+    </div>
   );
 }
-
 export default TwitModal;
